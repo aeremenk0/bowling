12e79db3123413bbb79fcf8813055d2257c67998
Anton Erememko <anton@pave.com>
2017-11-21 13:52:17 -0500

    step 3: make calc to handle all cases.

--- src/main/scala/ru/eremenko/bowling/Bowling.scala
+++ src/main/scala/ru/eremenko/bowling/Bowling.scala
@@ -9,24 +9,26 @@ case class LastFrame(first: Int, second: Int, third: Option[Int]) extends Frame
 case class Accumulator(score: Int, last: Option[Int], secondLast: Option[Int])
 
 object Bowling {
-  def calc(frame: Frame, acc: Accumulator): Accumulator =
+  def calc(frame: Frame, acc: Accumulator): Either[String, Accumulator] =
     frame match {
       case Strike(f) =>
         acc match {
           case Accumulator(score, Some(last), Some(secondLast)) =>
-            Accumulator(score + f + last + secondLast, Some(f), Some(last))
+            Right(Accumulator(score + f + last + secondLast, Some(f), Some(last)))
           case Accumulator(score, sl, None) =>
-            Accumulator(score, Some(f), sl)
+            Right(Accumulator(score, Some(f), sl))
+          case _ => Left("Accumulator invariant violation")
         }
       case Spare(f, s) =>
         acc match {
           case Accumulator(score, Some(last), _) =>
-            Accumulator(score + f + s + last, Some(f), Some(last))
+            Right(Accumulator(score + f + s + last, Some(f), Some(last)))
           case Accumulator(score, None, None) =>
-            Accumulator(score, Some(f+s), None)
+            Right(Accumulator(score, Some(f+s), None))
+          case _ => Left("Accumulator invariant violation")
         }
-      case Regular(f, s) => Accumulator(acc.score + f + s, Some(f), Some(s))
-      case LastFrame(f, s, t) => Accumulator(acc.score + f + s + t.getOrElse(0), Some(f), Some(s))
+      case Regular(f, s) => Right(Accumulator(acc.score + f + s, Some(f), Some(s)))
+      case LastFrame(f, s, t) => Right(Accumulator(acc.score + f + s + t.getOrElse(0), Some(f), Some(s)))
     }
 
   def isLastFrame(fr: Frame): Boolean = {
@@ -60,9 +62,11 @@ object Bowling {
     else None
   }
 
-  def score(game: List[Frame]): Int = {
-    val acc = game.foldRight(Accumulator(0, None, None))(calc(_,_))
+  def score(game: List[Frame]): Either[String, Int] = {
+    val acc = game.foldRight(Right(Accumulator(0, None, None)): Either[String, Accumulator]){
+      (fr, acc) =>  acc.flatMap(a => calc(fr, a))
+    }
 
-    acc.score
+    acc.map(_.score)
   }
 }
--- src/test/scala/ru/eremenko/bowling/BowlingSpec.scala
+++ src/test/scala/ru/eremenko/bowling/BowlingSpec.scala
@@ -5,48 +5,48 @@ import org.scalatest.{FlatSpec, Matchers}
 class BowlingSpec extends FlatSpec with Matchers {
   "Bowling.score" should "return score 0 for an empty input" in {
     val r = Bowling.score(List.empty)
-    r shouldBe 0
+    r shouldBe Right(0)
   }
 
   "Bowling.score" should "return score 30 for LastFrame of strikes" in {
     val r = Bowling.score(List(LastFrame(10,10,Option(10))))
-    r shouldBe 30
+    r shouldBe Right(30)
   }
 
   "Bowling.score" should "return score 9 for Regular frame" in {
     val r = Bowling.score(List(LastFrame(4,5,None)))
-    r shouldBe 9
+    r shouldBe Right(9)
   }
 
   "Bowling.score" should "return score 19 for Regular + Spare sequence" in {
     val r = Bowling.score(List(Spare(5,5), Regular(2,5)))
-    r shouldBe (12 + 7)
+    r shouldBe Right(12 + 7)
   }
 
   "Bowling.score" should "return score 21 for Regular + Strike sequence" in {
     val r = Bowling.score(List(Strike(), Regular(2,5)))
-    r shouldBe (10 + 2 + 5 + 7)
+    r shouldBe Right(10 + 2 + 5 + 7)
   }
 
   "Bowling.score" should "return score 300 for all Strikes sequence" in {
     val game = ( LastFrame(10, 10, Option(10)) :: List.fill(9)(Strike())).reverse
 
     val r = Bowling.score(game)
-    r shouldBe 300
+    r shouldBe Right(300)
   }
 
   "Bowling.score" should "return score 90 for all 10xRegular(4,5) sequence" in {
     val game = ( LastFrame(4, 5, None) :: List.fill(9)(Regular(4,5))).reverse
 
     val r = Bowling.score(game)
-    r shouldBe 90
+    r shouldBe Right(90)
   }
 
   "Bowling.score" should "return score 150 for all Spare(5,5) sequence" in {
     val game = ( LastFrame(5, 5, Option(5)) :: List.fill(9)(Spare(5,5))).reverse
 
     val r = Bowling.score(game)
-    r shouldBe 150
+    r shouldBe Right(150)
   }
 
   "Bowling.isLastFrame" should "return true for LastFrame" in {
@@ -104,36 +104,36 @@ class BowlingSpec extends FlatSpec with Matchers {
 
   "Bowling.score" should "return 125 for partial game of Spares" in {
     val g = (List.fill(9)(Spare(5,5))).reverse
-    Bowling.score(g) shouldBe 125
+    Bowling.score(g) shouldBe Right(125)
   }
 
   "Bowling.score" should "return 270 for partial game of Strikes" in {
     val g = (List.fill(9)(Strike())).reverse
-    Bowling.score(g) shouldBe 210
+    Bowling.score(g) shouldBe Right(210)
   }
 
   "Bowling.score" should "return 10 for partial game of Regular" in {
     val g = List(Regular(1,2), Regular(3, 4))
-    Bowling.score(g) shouldBe 10
+    Bowling.score(g) shouldBe Right(10)
   }
 
   "Bowling.score" should "return 10 for partial game of Regular + Strike" in {
     val g = List(Regular(1,2), Regular(3, 4), Strike())
-    Bowling.score(g) shouldBe 10
+    Bowling.score(g) shouldBe Right(10)
   }
 
   "Bowling.score" should "return 30 for partial game of Regular + Strike + Regular" in {
     val g = List(Regular(1,2), Regular(3, 4), Strike(), Regular(2, 3))
-    Bowling.score(g) shouldBe 30
+    Bowling.score(g) shouldBe Right(30)
   }
 
   "Bowling.score" should "return 10 for partial game of Regular + Spare" in {
     val g = List(Regular(1,2), Regular(3, 4), Spare(5, 5))
-    Bowling.score(g) shouldBe 10
+    Bowling.score(g) shouldBe Right(10)
   }
 
   "Bowling.score" should "return 14 for partial game of Regular + Spare + Regular" in {
     val g = List(Regular(1,2), Regular(3, 4), Spare(5, 5), Regular(2,3))
-    Bowling.score(g) shouldBe 27
+    Bowling.score(g) shouldBe Right(27)
   }
 }
